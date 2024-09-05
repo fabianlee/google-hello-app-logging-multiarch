99184c634f32528df9453c31df5fd51e6bd4a809
Fabian Lee <flee999@hotmail.com>
2024-09-05 17:17:25 -0400

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# 
+
+This project is directly based on the code from the [GoogleCloudPlatform/kubernetes-engine-samples/quickstarts/hello-app](https://github.com/GoogleCloudPlatform/kubernetes-engine-samples/tree/main/quickstarts/hello-app)
+
+The enhancement in this project is building this GoLang app with multiple architectural targets, so that it can also run on ARM64, specifically Apple Silicon as well as AMD64.
+
+## Github pipeline and published image
+
+The github pipeline takes care of the multi-arch build, and publishes the image to the Github Container Registry.
+
+
+```
+docker pull ghcr.io/fabianlee/google-hello-app-logging-multiarch:1.0
+```
+
+## Creating tag that invokes Github Action
+
+```
+newtag=v1.0.1
+git commit -a -m "changes for new tag $newtag" && git push -o ci.skip
+git tag $newtag && git push origin $newtag
+```
+
+## Deleting tag
+
+```
+# delete local tag, then remote
+todel=v1.0.1
+git tag -d $todel && git push -d origin $todel
+```
+
---
+++ main.go
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2021 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// [START gke_hello_app]
+// [START container_hello_app]
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+var loopIndex int64 = 0
+var whoAmI string = "world"
+
+func main() {
+        //log.SetFlags(log.Ldate);
+
+	// any override?
+	if os.Getenv("whoAmI") != "" {
+	   whoAmI = os.Getenv("whoAmI")
+	}
+        // run log message once a second
+	go runDataLoop()
+
+	// register hello function to handle all requests
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+
+	// use PORT environment variable, or default to 8080
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// start the web server on port and accept requests
+	log.Printf("Server listening on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, mux))
+}
+
+// hello responds to the request with a plain-text "Hello, world" message.
+func hello(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Serving request: %s", r.URL.Path)
+	fmt.Fprintf(w, "Hello, %s!\n",whoAmI)
+}
+
+func runDataLoop() {
+    for {
+        loopIndex++
+	slog.Info("logging at info level",whoAmI)
+	slog.Warn("logging at warn level",whoAmI)
+	slog.Error("logging at err level",whoAmI)
+        time.Sleep(1 * time.Second)
+    }
+}
+
+// [END container_hello_app]
+// [END gke_hello_app]
